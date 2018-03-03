094f0d03447cd3fa0d2ddad0ce94a5e164e8ce06
Christof Neumann <christofneumann1@gmail.com>
2018-03-03 23:15:02 +0900

    added lme4 support to resample function, added some data checks in resample function

--- R/resamplefunction.R
+++ R/resamplefunction.R
@@ -14,12 +14,14 @@
 #' @importFrom parallel detectCores makeCluster stopCluster
 #' @importFrom iterators icount
 #' @importFrom stats predict update
+#' @importFrom lme4 ranef
 #' @return a list with two items:
 #'  \itemize{
 #'   \item a \code{data.frame} with the predictor variables at the values for which the prediction was made
 #'   \item a \code{matrix} with the same number of rows as the first element of the list, and \code{N} columns, i.e. the predicted values for each sample
 #'  }
 #' @export
+#' @details the function needs data sets in which columns have one dimension only (at least on the predictor side). If you get a warning or error, please check \code{unlist(lapply(<yourdata>, ncol))} to find columns that have more than one dimension. This could happen, for example, if you applied \code{scale()} to a numerical column. You could use \code{<yourdata>$mycol <- as.numeric(<yourdata>$mycol)} to fix this issue. Also, the model has to be refitted after such an operation.
 #' @examples
 #' \dontrun{
 #' library(glmmADMB)
@@ -42,48 +44,123 @@
 #' resamplefunction(res, xdata, N = 2, termsref = "pred2")
 #' # keep $pred1 at reference and $pred3 at 0
 #' resamplefunction(res, xdata, N = 2, termsref = c("pred1", "pred3"))
+#'
+#' epil2$subject <- factor(epil2$subject)
+#' (fm <- glmmadmb(y ~ Base*trt + Age + Visit + (Visit|subject),
+#'                 data = epil2, family = "nbinom"))
+#' resamplefunction(model = fm, dat = epil2, N = 3, stepsalong = 3)
+#'
+#' library(lme4)
+#' (fm1 <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy))
+#' resamplefunction(model = fm1, dat = sleepstudy, N = 3, stepsalong = 3)
+#'
+#' (gm1 <- glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
+#'               data = cbpp, family = binomial))
+#' resamplefunction(model = gm1, dat = cbpp, N = 3, stepsalong = 3)
 #' }
 #'
 resamplefunction <- function(model, dat, N, termsref = NULL, useparallel = TRUE, stepsalong = 11, allcores = FALSE) {
-  # take all terms to vary
-  xterms <- attr(x = model$terms, which = "term.labels")
-  # get data classes
-  dc <- attr(x = model$terms, which = "dataClasses")
-  # get main effects names
-  xterms <- dc[names(dc) %in% xterms]
+  # sometimes, data columns in the data set might be matrices with 1 column (which appear as 'nmatrix.1' data classes in the model objects)
+  # eg. when data were subjected to 'scale()'
+  # for now, make sure that columns are vectors (not matrices)
+  if(class(model) == "lmerMod" | class(model) == "glmerMod") {
+    datclasses <- as.character(unlist(lapply(model@frame, class)))
+  }
+  if(class(model) == "glmmadmb") {
+    datclasses <- as.character(unlist(lapply(model$frame, class)))
+  }
+  if("matrix" %in% datclasses) {
+    warning("You have at least one matrix in your data set. Please transform into a vector.", call. = FALSE)
+  }
+  rm(datclasses)
+  # tempclasses <- unlist(lapply(dat, class))
+  # if("matrix" %in% tempclasses) {
+  #   repl <- which(tempclasses == "matrix")
+  #   for(i in repl) {
+  #     dat[, names(tempclasses)[i]] <- as.numeric(dat[, names(tempclasses)[i]])
+  #   }
+  #   model <- update(model, data = dat)
+  # }
+  # rm(tempclasses, repl)
+
+
+
+  # get the terms and combinations of values for the prediction, depending on model type (glmmadmb or lmer/glmer)
+
+  if(class(model) == "lmerMod" | class(model) == "glmerMod") {
+    # m <- model@frame
+    # str(m)
+    # attr(m, "terms")
+    # unlist(attr(attr(m, "terms"), "predvars.random"))
+
+    # all terms for predictor variables (including interactions)
+    allterms <- attr(attr(model@frame, "terms"), "term.labels")
+    # data classes (for main effects, but also includes the response and the random effects [and maybe even an offset if present?])
+    datclasses <- attr(attr(model@frame, "terms"), "dataClasses")
+    # get main effect names (still includes random effects)
+    prednames <- datclasses[names(datclasses) %in% allterms]
+    # exclude random effects
+    ref <- names(ranef(model))
+    for(i in ref) prednames <- prednames[-c(which(names(prednames) == i))]
+  }
+
+  if(class(model) == "glmmadmb") {
+    # all terms for predictor variables (including interactions)
+    allterms <- attr(x = model$terms, which = "term.labels")
+    # data classes (for main effects, but also includes the response [and maybe even an offset if present?])
+    datclasses <- attr(x = model$terms, which = "dataClasses")
+    # get main effect names
+    prednames <- datclasses[names(datclasses) %in% allterms]
+
+    # # take all terms to vary
+    # xterms <- attr(x = model$terms, which = "term.labels")
+    # # get data classes
+    # dc <- attr(x = model$terms, which = "dataClasses")
+    # # get main effects names
+    # xterms <- dc[names(dc) %in% xterms]
+  }
+
+
+
   # get values for predictions depending on the term's data class
   predvals <- list()
-  for(i in 1:length(xterms)) {
-    if(xterms[i] == "factor") {
-      predvals[[length(predvals) + 1]] <- levels(dat[, names(xterms)[i]])
+
+  for(i in 1:length(prednames)) {
+    if(prednames[i] == "factor") {
+      predvals[[length(predvals) + 1]] <- levels(dat[, names(prednames)[i]])
     }
-    if(xterms[i] == "numeric" | xterms[i] == "integer") {
-      predvals[[length(predvals) + 1]] <- seq(from = min(dat[, names(xterms)[i]]), to = max(dat[, names(xterms)[i]]), length.out = stepsalong)
+    if(prednames[i] == "numeric" | prednames[i] == "integer") {
+      # if a numeric variable has only two realizations, most commonly 0 and 1, assume it's actually categorical (e.g. manual contrast coding)
+      if(length(table(dat[, names(prednames)[i]])) == 2) {
+        predvals[[length(predvals) + 1]] <- as.numeric(names(table(dat[, names(prednames)[i]])))
+      } else {
+        predvals[[length(predvals) + 1]] <- seq(from = min(dat[, names(prednames)[i]]), to = max(dat[, names(prednames)[i]]), length.out = stepsalong)
+      }
     }
-    names(predvals)[i] <- names(xterms)[i]
+    # assign name to the list element
+    names(predvals)[i] <- names(prednames)[i]
   }
 
-  # if terms were specified, take those that are not specified at value 0 (for numeric) or reference level (for factor)
+  # if 'termsref' were specified, take those at value 0 (for numeric), reference level (for factor) or at it's smaller value in case of two numerical values
   if(!is.null(termsref)) {
     for(i in 1:length(termsref)) {
-      if(dc[termsref[i]] == "numeric" | dc[termsref[i]] == "integer") {
-        predvals[termsref[i]] <- 0
+      if(datclasses[termsref[i]] == "numeric" | datclasses[termsref[i]] == "integer") {
+        if(length(table(dat[, termsref[i]])) == 2) {
+          predvals[termsref[i]] <- as.numeric(names(table(dat[, termsref[i]])))[1]
+        } else {
+          predvals[termsref[i]] <- 0
+        }
       }
-      if(dc[termsref[i]] == "factor") {
-        predvals[termsref[i]] <- levels(dat[, termsref[i]])[1]
+      if(datclasses[termsref[i]] == "factor") {
+        # make sure that the variable remains a factor
+        # predvals[termsref[i]] <- factor(levels(dat[, termsref[i]]))
+        predvals[termsref[i]] <- factor(levels(dat[, termsref[i]])[1], levels = levels(dat[, termsref[i]]))
       }
     }
   }
 
+  # make a data frame for combinations of values to predict over (only main effects)
   ndata <- expand.grid(predvals)
-  # re'factor' levels
-  if("factor" %in% xterms) {
-    xnames <- names(xterms)[xterms == "factor"]
-    for(i in xnames) {
-      ndata[, i] <- factor(ndata[, i], levels = levels(dat[, i]))
-    }
-  }
-
 
   if(useparallel) {
     # initiate multicore
@@ -95,25 +172,40 @@ resamplefunction <- function(model, dat, N, termsref = NULL, useparallel = TRUE,
     }
     registerDoParallel(cl)
 
-    allres <- foreach(1:N, .packages = "glmmADMB", .combine = 'cbind') %dopar% {
-      tempdata <- dat
-      tempdata <- tempdata[sample(1:nrow(tempdata), replace = TRUE), ]
-      xres <- update(model, data = tempdata)
-      predict(xres, newdata = ndata, re.form = NULL, type = "response")
+    if(class(model) == "lmerMod" | class(model) == "glmerMod") {
+      allres <- foreach(1:N, .packages = "lme4", .combine = 'cbind') %dopar% {
+        tempdata <- dat
+        tempdata <- tempdata[sample(1:nrow(tempdata), replace = TRUE), ]
+        xres <- update(model, data = tempdata)
+        predict(xres, newdata = ndata, re.form = NA, type = "response")
+      }
+      on.exit(stopCluster(cl))
     }
-    on.exit(stopCluster(cl))
+    if(class(model) == "glmmadmb") {
+      allres <- foreach(1:N, .packages = "glmmADMB", .combine = 'cbind') %dopar% {
+        tempdata <- dat
+        tempdata <- tempdata[sample(1:nrow(tempdata), replace = TRUE), ]
+        xres <- update(model, data = tempdata)
+        predict(xres, newdata = ndata, re.form = NA, type = "response")
+      }
+      on.exit(stopCluster(cl))
+    }
+
   }
 
+  # if parallel is turned off use a simple loop
   if(!useparallel) {
     allres <- matrix(ncol = N, nrow = nrow(ndata))
     for(i in 1:N) {
       tempdata <- dat
       tempdata <- tempdata[sample(1:nrow(tempdata), replace = TRUE), ]
       xres <- update(model, data = tempdata)
-      allres[, i] <- predict(xres, newdata = ndata, re.form = NULL, type = "response")
+      allres[, i] <- predict(xres, newdata = ndata, re.form = NA, type = "response")
     }
   }
 
-  ndata$orifit <- predict(model, newdata = ndata, re.form = NULL, type = "response")
+  # predict for original data
+  ndata$orifit <- predict(model, newdata = ndata, re.form = NA, type = "response")
   return(list(ndata, allres))
 }
+
